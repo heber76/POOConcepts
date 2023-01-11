daa205f70004c277662c30f53a1cf3c1f77eee36
heber ochoa prieto <heber76@hotmail.com>
2023-01-11 15:16:03 -0600

    Finish date class

---
+++ POOConcepts/POOConcepts/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using POOConcepts;
+
+Console.WriteLine("POO Concepts");
+Console.WriteLine("=========================");
+
+//Date date1 = new Date(2023,1,11);
+try
+{
+	Console.WriteLine(new Date(2024, 2, 29));
+	Console.WriteLine(new Date(1974, 9, 23
+		));
+	Console.WriteLine(new Date(1985, 11, 30));
+}
+catch (Exception error)
+{
+
+	Console.WriteLine(error.Message);
+}
+
+
+
+
